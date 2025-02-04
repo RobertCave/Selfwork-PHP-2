3a007676b8449e8870179f8bd5ec2bb650e323ba
Roberto Lacava <roberto.lacava@gmail.com>
2025-02-04 11:45:36 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Selfwork-PHP-2
+# Selfwork-PHP-2
---
+++ php_02_roberto_lacava.php
@@ -0,0 +1,35 @@
+<?php
+//  Selfwork PHP 2
+// Date le seguenti variabili:
+
+//     $1text = "Marco";
+//     $text2 = "hai";
+//     $text.3 = "sete";
+//     $text4 = "?";
+//     @text5 = "Perchè";
+//     $te-xt6 = '$text2';
+//     $text 7 = 'bevuto';
+//     $text8 = "tutto"
+
+
+// correggi eventuali errori e stampa correttamente a terminale la stringa: “Marco Hai sete? Perche' hai bevuto tutto.“ -->
+
+
+
+
+
+
+
+    $text = "Marco";
+    $text2 = "hai";
+    $text3 = "sete";
+    $text4 = "?";
+    $text5 = "Perchè";
+    $text6 = '$text2';
+    $text7 = 'bevuto';
+    $text8 = "tutto"; 
+
+
+    echo "$text $text2 $text3$text4 $text5 $text2 $text7 $text8.";
+    
+?>
